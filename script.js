b8d92fb5b30e13e324ee35716296935ca8865d27
phatss-dev <phunghuuphat3@gmail.com>
2025-05-30 19:46:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,8 @@ function tapLove(id) {
       if (!loveMsg) return console.error('Không tìm thấy element loveMsg!');
 
       typeWriterEffect(
-        `Chúc ${userName} của anh 1/6 thật vui vẻ như một đứa trẻ, nhưng được anh yêu như một nữ hoàng 👑. Dù em có lớn bao nhiêu thì trong tim anh, em vẫn là công chúa bé bỏng cần được cưng chiều mỗi ngày! 💘`,
+        `Chúc ${userName} của anh 1/6 thật vui vẻ như một đứa trẻ, nhưng được anh yêu như một nữ hoàng 👑. Dù em có lớn bao nhiêu thì trong tim anh, em vẫn là cô công chúa bé bỏng cần được cưng chiều mỗi ngày! 💘.
+        YÊU CÔ CÔNG CHÚA NHỎ CỦA ANH 😘`,
         'loveMsg',
         () => {
           const fromTag = document.createElement("div");
